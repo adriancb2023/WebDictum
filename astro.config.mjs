02246456561a3f15c78c380888dfb9ff52af2bca
Francisco José Navarro Ruiz <142453327+KatEston23@users.noreply.github.com>
2025-06-18 14:12:26 +0200

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/server';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server'
+  output: 'server',
+  adapter: vercel(),
 }); 
